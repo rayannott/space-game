5d1a0a787351b611d625fb4129d74230e4b5d569
Airat-ASMPT-PC <airat.valiullin@asmpt.com>
2024-10-29 10:44:03 +0100

    thrust animation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from typing import Literal
@@ -14,6 +15,7 @@ WHITE = Color("white")
 MAGENTA = Color("magenta")
 GREEN = Color("green")
 GRAY = Color("gray")
+CYAN = Color("cyan")
 
 ACCELERATION_ROTATION_PER_SCROLL = 20
 FRICTION_PER_SECOND = 0.05  # vel amplitude is decreased by 5% per second
@@ -50,20 +52,22 @@ class Entity(ABC):
 
 @dataclass
 class Engine:
-    strength: int
     _on: bool = False
     _speedup: bool = False
 
     def get(self) -> int:
         if not self._on:
             return 0
-        return self.strength if self._speedup else 1
+        return 9 if self._speedup else 3
 
     def on(self) -> None:
         self._on = True
 
     def off(self) -> None:
         self._on = False
+
+    def is_speedup_on(self) -> bool:
+        return self._speedup
 
     def set_speedup(self, speedup: bool) -> None:
         self._speedup = speedup
@@ -75,7 +79,7 @@ class Engine:
 class Player(Entity):
     def __init__(self, pos: Vector2, vel: Vector2, acc: Vector2):
         super().__init__(pos, vel, PLAYER_SIZE, acc)
-        self.engine = Engine(strength=5)
+        self.engine = Engine()
 
     def update(self, time_delta: float):
         self.pos += self.vel * time_delta
@@ -91,8 +95,8 @@ class Game:
         self.surface_rect = surface_rect
         self.center = Vector2(surface_rect.center)
         self.player = Player(
-            Vector2(600.0, 200.0),
-            Vector2(0.0, 0.0),
+            self.center.copy(),
+            Vector2(),
             PLAYER_ACC_AMPLITUDE * random_unit_vector(),
         )
 
@@ -112,14 +116,20 @@ class Game:
 
 
 class GameScreen(Screen):
-    def __init__(self, surface: Surface, control_type: Literal['scroll', 'cursor'] = 'cursor'):
+    def __init__(
+        self,
+        surface: Surface,
+        control_type: Literal["scroll", "cursor"] = "cursor",
+        debug: bool = False,
+    ):
         super().__init__(surface)
         self.game = Game(surface.get_rect())
         self.control_type = control_type
+        self.debug = debug
 
     def process_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.control_type == 'scroll' and event.button in {4, 5}:
+            if self.control_type == "scroll" and event.button in {4, 5}:
                 self.game.player.rotate_acc(
                     (1 if event.button == 4 else -1) * ACCELERATION_ROTATION_PER_SCROLL
                 )
@@ -132,15 +142,31 @@ class GameScreen(Screen):
                 self.game.player.engine.off()
             elif event.button == 3:
                 self.game.player.engine.set_speedup(False)
-        elif self.control_type == 'cursor' and event.type == pygame.MOUSEMOTION:
+        elif self.control_type == "cursor" and event.type == pygame.MOUSEMOTION:
             acc_magn = self.game.player.acc.magnitude()
-            self.game.player.acc = (get_mouse_pos() - self.game.center).normalize() * acc_magn
+            self.game.player.acc = (
+                get_mouse_pos() - self.game.center
+            ).normalize() * acc_magn
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self.is_running = False
 
     def update(self, time_delta):
         self.game.update(time_delta)
+        
+        # thrust animation
+        if self.game.player.engine:
+            speedup = self.game.player.engine.is_speedup_on()
+            point_behind = self.game.player.pos - self.game.player.acc.normalize() * (100 if speedup else 30)
+            for _ in range(10 if speedup else 5):
+                color = random.choice([WHITE, MAGENTA, CYAN])
+                pygame.draw.line(
+                    self.surface,
+                    color,
+                    self.game.player.pos,
+                    point_behind + random_unit_vector() * random.uniform(2, 5),
+                    width=6 if speedup else 2,
+                )
 
         # drawing
         pygame.draw.circle(
@@ -149,23 +175,25 @@ class GameScreen(Screen):
             self.game.player.pos,
             10,
         )
-        pygame.draw.line(
-            self.surface,
-            GRAY if not self.game.player.engine else MAGENTA,
-            self.game.player.pos,
-            self.game.player.pos + self.game.player.acc.normalize() * 30,
-            width=self.game.player.engine.get() * 2 + 1,
-        )
-        pygame.draw.line(
-            self.surface,
-            GREEN,
-            self.game.player.pos,
-            self.game.player.pos + self.game.player.vel * 0.1,
-            width=2,
-        )
+        if self.debug:
+            pygame.draw.line(
+                self.surface,
+                GRAY if not self.game.player.engine else MAGENTA,
+                self.game.player.pos,
+                self.game.player.pos + self.game.player.acc.normalize() * 30,
+                width=self.game.player.engine.get() * 2 + 1,
+            )
+            pygame.draw.line(
+                self.surface,
+                GREEN,
+                self.game.player.pos,
+                self.game.player.pos + self.game.player.vel * 0.1,
+                width=2,
+            )
+
 
         # cursor controls
-        if self.control_type == 'cursor':
+        if self.control_type == "cursor":
             pygame.draw.circle(self.surface, GREEN, self.game.center, 40, 2)
             pygame.draw.circle(
                 self.surface,
@@ -178,7 +206,7 @@ class GameScreen(Screen):
 def main():
     pygame.init()
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    game_screen = GameScreen(screen, control_type='cursor')
+    game_screen = GameScreen(screen, control_type="cursor")
     game_screen.run()
     pygame.quit()
 
